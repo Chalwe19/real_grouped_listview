26ca042dff1719060a4e9a4a2adc1dbfc4c5b097
chalwe19 <cchisela19@gmail.com>
2023-07-06 10:44:24 +0200

    Initial commit

---
+++ lib/real_grouped_listview.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class RealGroupedList extends StatefulWidget {
+  const RealGroupedList({super.key});
+
+  @override
+  State<RealGroupedList> createState() => _RealGroupedListState();
+}
+
+class _RealGroupedListState extends State<RealGroupedList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
