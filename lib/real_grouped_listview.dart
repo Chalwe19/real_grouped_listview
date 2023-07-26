f4b030a15917eff6c4e6fbfac3100d972848e5e9
chalwe19 <cchisela19@gmail.com>
2023-07-26 15:41:28 +0200

    update
    Signed-off-by: chalwe19 <cchisela19@gmail.com>

--- lib/real_grouped_listview.dart
+++ lib/real_grouped_listview.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class RealGroupedList extends StatefulWidget {
-  const RealGroupedList({super.key});
+class RealGroupedList<T, E> extends StatefulWidget {
+  ///List items
+  final int itemCount;
+  final List<T> elements;
+
+  const RealGroupedList({
+    super.key,
+    required this.itemCount,
+    required this.elements,
+  });
 
   @override
   State<RealGroupedList> createState() => _RealGroupedListState();
 }
 
-class _RealGroupedListState extends State<RealGroupedList> {
+class _RealGroupedListState<T, E> extends State<RealGroupedList> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.separated(
+      itemCount: 100,
+      separatorBuilder: (context, index) => const SizedBox(height: 10),
+      itemBuilder: (context, index) => Container(
+        height: 100,
+        width: 100,
+        color: index % 2 == 0 ? Colors.red : Colors.blue,
+      ),
+    );
   }
 }
